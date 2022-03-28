3a8485d3afade3f9e0944f7cedd0f37e629bb019
Elias Birkedal <eliasb@uia.no>
2022-03-28 15:52:45 +0200

    created urlChecker module

---
+++ Modules/_urlChecker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from ..bot import re
+
+#url_pattern = "/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/"
+url_pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+url_regex = re.compile(url_pattern)
+
+def url_search(message):
+    if not url_regex.search(message):
+        return False
+
+
+    
--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import re
 
 from discord.ext import commands
 from dotenv import load_dotenv
+
+from .Modules._urlChecker import url_search
 
 load_dotenv()
 
@@ -11,10 +14,19 @@ DISCORD_API = os.environ.get("DISCORD_API")
 
 bot = commands.Bot(command_prefix='!')
 
+# URL regex
+# https://stackoverflow.com/questions/1410311/regular-expression-for-url-validation-in-javascript/1411800#1411800
+# /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+
 @bot.event
 async def on_ready():
     print("="*50)
     print('Im in, as {0.user}'.format(bot))
     print("="*50)
 
+@bot.event
+async def on_message(message):
+    pass
 
+
+bot.run(DISCORD_API)
